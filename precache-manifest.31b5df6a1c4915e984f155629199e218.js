1914d07a904fc7349bf9c54a820aba2d5281c66c
Uday Kumar <udayka014@gmail.com>
2022-07-11 09:54:45 +0530

    Updates

---
+++ precache-manifest.31b5df6a1c4915e984f155629199e218.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757fd8c0c8a60b8a9beaca9ad4d7a374",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "0751ac552563581fd2c7",
+    "url": "/Keeper/static/js/2.419013f1.chunk.js"
+  },
+  {
+    "revision": "0f7fa04528c2725b814b",
+    "url": "/Keeper/static/js/main.cec7efdf.chunk.js"
+  },
+  {
+    "revision": "2d4c3f94dc5fce9c3a57",
+    "url": "/Keeper/static/js/runtime-main.261ee5b7.js"
+  }
+]);
